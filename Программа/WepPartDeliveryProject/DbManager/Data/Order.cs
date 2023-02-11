d32be93d902c911bec8249fe8a0379f5f1b1097b
Valdemar <jiltsov01@yandex.ru>
2023-02-11 10:52:19 +0300

    Добавление новых моделей (DeliveryMan, Order, Client)

---
+++ Программа/WepPartDeliveryProject/DbManager/Data/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Data
+{
+    public class Order:IModel
+    {
+        public double Price { get; set; }
+        public List<OrderedObject> OrderedObjects { get; set; }
+        public Person
+    }
+}
