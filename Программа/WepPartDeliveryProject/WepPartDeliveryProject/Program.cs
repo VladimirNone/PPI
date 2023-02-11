1bd5965a9f4014773acf3770fc97ba3449d1eae6
Valdemar <jiltsov01@yandex.ru>
2023-02-11 14:18:26 +0300

    Добавлены новые пути

--- Программа/WepPartDeliveryProject/WepPartDeliveryProject/Program.cs
+++ Программа/WepPartDeliveryProject/WepPartDeliveryProject/Program.cs
@@ -58,4 +58,19 @@ app.Map("/time", appBuilder =>
     appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
 });
 
+app.Map("/delivary", appBuilder =>
+{
+    var time = DateTime.Now.ToShortTimeString();
+
+    // логгируем данные - выводим на консоль приложения
+    appBuilder.Use(async (context, next) =>
+    {
+        Console.WriteLine($"Time: {time}");
+        await next();   // вызываем следующий middleware
+    });
+
+    appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
+});
+
+
 app.Run();
