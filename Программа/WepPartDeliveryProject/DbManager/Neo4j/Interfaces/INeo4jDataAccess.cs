da18433e610d0e50c5134e553201a8435b1c1730
Valdemar <jiltsov01@yandex.ru>
2023-02-11 10:26:05 +0300

    Added project

---
+++ Программа/WepPartDeliveryProject/DbManager/Neo4j/Interfaces/INeo4jDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Neo4j.Interfaces
+{
+    public interface INeo4jDataAccess : IAsyncDisposable
+    {
+        Task<List<string>> ExecuteReadListAsync(string query, string returnObjectKey, IDictionary<string, object>? parameters = null);
+
+        Task<List<Dictionary<string, object>>> ExecuteReadDictionaryAsync(string query, string returnObjectKey, IDictionary<string, object>? parameters = null);
+
+        Task<T> ExecuteReadScalarAsync<T>(string query, IDictionary<string, object>? parameters = null);
+
+        Task<T> ExecuteWriteTransactionAsync<T>(string query, IDictionary<string, object>? parameters = null);
+    }
+}
---
+++ Программа/WepPartDeliveryProject/DbManager/Neo4j/Interfaces/IPersonRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager.Neo4j.Interfaces
+{
+    public interface IPersonRepository
+    {
+    }
+}
